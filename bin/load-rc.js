63c3840aa91de3e34b1e83d92f7c2406625098af
Roger Steve Ruiz <hi@rog.gr>
2015-06-30 10:59:45 -0400

    Updating syntax, adding hinting

--- bin/load-rc.js
+++ bin/load-rc.js
@@ -1,48 +1,51 @@
-var fs = require('fs');
-var RSVP = require('rsvp');
-var path = require('path');
+var fs = require( 'fs' );
+var RSVP = require( 'rsvp' );
+var path = require( 'path' );
+
+var helpfulErrorMessage = 'Please verify that the .pantheonrc file is installed correctly';
 
 var loadRc = {
-  homeDir: new RSVP.Promise(function(resolve, reject) {
-    var homeDir = path.join(process.env.HOME, '.pantheonrc');
-    fs.readFile(homeDir, function(error, projects) {
-      if (error) {
-        reject(error);
+  homeDir: new RSVP.Promise( function( resolve, reject ) {
+    var homeDir = path.join( process.env.HOME, '.pantheonrc' );
+    fs.readFile( homeDir, function(error, projects) {
+      if ( error ) {
+        reject( error );
       }
-      resolve(projects);
+      resolve( projects );
     });
   }),
-  workingDir: new RSVP.Promise(function(resolve, reject) {
+  workingDir: new RSVP.Promise( function( resolve, reject ) {
     // TODO: Resolve this to the project root, not where the command was run from.
     // Not sure how to do this, though. Yet.
-    fs.readFile('./.pantheonrc', function(error, projects) {
-      if (error) {
-        reject(error);
+    // var workingDir = path.join('', '.pantheonrc');
+    fs.readFile( './.pantheonrc', function( error, projects ) {
+      if ( error ) {
+        reject( error );
       }
-      resolve(projects);
-    });
-  })
+      resolve( projects );
+    } );
+  } )
 };
 
 module.exports = RSVP.hashSettled(loadRc).then(function(attempts) {
   var projects;
-  if (attempts.workingDir.state === 'fulfilled') {
+  if ( 'fulfilled' === attempts.workingDir.state ) {
     projects = attempts.workingDir.value.toString();
-  } else if (attempts.homeDir.state === 'fulfilled') {
+  } else if ( 'fulfilled' === attempts.homeDir.state ) {
     projects = attempts.homeDir.value.toString();
   } else {
-    console.error('❯ %s not found', attempts.workingDir.reason.path);
-    console.error('❯ %s not found', attempts.homeDir.reason.path);
-    console.log('please verify .pantheonrc is installed correctly, and run pw again.');
-    process.exit(1);
+    console.error( '❯ %s not found', attempts.workingDir.reason.path );
+    console.error( '❯ %s not found', attempts.homeDir.reason.path );
+    console.log( helpfulErrorMessage );
+    process.exit( 1 );
   }
 
   try {
-    return JSON.parse(projects);
+    return JSON.parse( projects );
   } catch (e) {
     if (/error/i.test(e)) {
-      console.error('Error parsing JSON!');
-      console.log('please verify .pantheonrc is installed correctly, and run pw again.');
+      console.error( 'Error parsing JSON!' );
+      console.log( helpfulErrorMessage );
     }
   }
 
